b56a1fd747bcb66175e0d108a7eedc11e605a5cb
Code-Themba <Siyethemba3541@gmail.com>
2025-07-13 11:24:43 +0200

    Initial Commit. Added index.html, ./js/main.js, and ./css/styles.css files. Added button and area for generated quote to be displayed. Added test code to the ./js/main.js file, to see if the set up is working. It is.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const displayQuoteArea = document.getElementById("quote-display-area");
+const getQuoteButton = document.getElementById("get-quote-button");
+
+// Function To Generate Quotes
+function getQuotes() {
+  console.log("This function returns randomly generated qoutes.");
+}
+
+getQuoteButton.addEventListener("click", (e) => {
+  getQuotes();
+});
